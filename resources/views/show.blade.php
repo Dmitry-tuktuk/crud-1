7d19daf852a39aec595300c2b074b8642d7729d8
Dmitry-tuktuk <57315387+Dmitry-tuktuk@users.noreply.github.com>
2021-07-01 13:16:37 +0300

    cart for User

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,26 @@
 @extends('layout')
 
-@section('title', 'Update')
+@section('title', 'User ' .$user->name)
 
 @section('content')
-    {{ dd($user) }}
+
+    <div class="card mt-3" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{$user->id}}</li>
+            <li class="list-group-item">Name: {{$user->name}}</li>
+            <li class="list-group-item">Email: {{$user->email}}</li>
+            <li class="list-group-item">Created: {{$user->created_at->format('d/m/y H:i:s')}}</li>
+            <li class="list-group-item">Updated: {{$user->updated_at->format('d/m/y H:i:s')}}</li>
+        </ul>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <a type="button" class="btn btn-secondary" href="{{route('users.index')}}">Back to users</a>
+        </div>
+        <div class="col-md-3 ">
+            <a type="button" class="btn btn-warning " href="{{route('users.edit', $user)}}">Edit</a>
+        </div>
+    </div>
+
+
 @endsection
